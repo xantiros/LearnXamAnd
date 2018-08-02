0c0f67c7945472012660c030490013c997ea0e2e
Xantiros <xantiros@gmail.com>
2018-08-02 16:10:49 +0200

    1 QuickStart

--- LearnXamAnd/MainActivity.cs
+++ LearnXamAnd/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.Support.V7.App;
 
 namespace LearnXamAnd
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "Phone Word", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -14,6 +14,24 @@ namespace LearnXamAnd
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatePhoneWord);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+
+            translateButton.Click += (sender, e) =>
+            {
+                string translatedNumber = Core.PhoneWordTranslator.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    translatedPhoneWord.Text = string.Empty;
+                }
+                else
+                {
+                    translatedPhoneWord.Text = translatedNumber;
+                }
+            };
+
         }
     }
 }
