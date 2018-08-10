719c3f11bbf9babc3c395b5f9d9ab0916611d5be
Xantiros <xantiros@gmail.com>
2018-08-10 15:31:58 +0200

    Incrementing App

---
+++ SimpleApp/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace SimpleApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        TextView txtNumber;
+        int number;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            txtNumber = FindViewById<TextView>(Resource.Id.txtNumber);
+
+            FindViewById<Button>(Resource.Id.btnIncrement).Click += (o, e) =>
+            txtNumber.Text = (++number).ToString();
+
+            FindViewById<Button>(Resource.Id.btnDecrement).Click += (o, e) =>
+            txtNumber.Text = (--number).ToString();
+        }
+    }
+}
+
